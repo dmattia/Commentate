b59de33c659a626217fab521e306c366fd2e2254
dmattia <dmattia@nd.edu>
2016-01-31 19:40:20 -0500

    Initial Commit

---
+++ MenuViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MenuViewController.swift
+//  Commentate
+//
+//  Created by David Mattia on 1/31/16.
+//  Copyright © 2016 David Mattia. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
